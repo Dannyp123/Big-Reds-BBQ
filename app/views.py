b084d71a07b3e27f6ff3e9608ae441dd15f82637
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-07-02 19:58:43 -0500

    First Commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+from app import forms
+from app import models
+
+# Create your views here.
+
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, "home.html")
